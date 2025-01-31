d5ab4b00d5fba73c6e31d4a0eba1308f70792888
Ivan Tustanivskyi <tustanivsky@gmail.com>
2025-01-31 12:11:38 +0200

    Fix errors related to sampling context

--- plugin-dev/Source/Sentry/Private/HAL/PlatformSentrySamplingContext.h
+++ plugin-dev/Source/Sentry/Private/HAL/PlatformSentrySamplingContext.h
@@ -10,11 +10,7 @@
 
 static TSharedPtr<ISentrySamplingContext> CreateSharedSentrySamplingContext()
 {
-#if PLATFORM_ANDROID
-    return MakeShareable(new SentrySamplingContextAndroid);
-#elif PLATFORM_APPLE
-    return MakeShareable(new SentrySamplingContextApple);
-#else
+    // Sampling context is supposed to be created internally by the SDK using the platform-specific implementations.
+    // Currently, it doesn't provide default constructor for Apple/Android thus we can only return Null-version here.
     return MakeShareable(new FNullSentrySamplingContext);
-#endif
 }
